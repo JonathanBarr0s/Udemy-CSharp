aa775c8aed0cb1abcd1522c087bf6b645851aae1
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2024-03-13 00:04:36 -0300

    Att

---
+++ 00. Recapitulação de Lógica de Programação/17. Os Ímpares/NumerosImpares/NumerosImpares/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumerosImpares {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite um número: ");
+            int numeroDigitado = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 0; i < (numeroDigitado + 1); i++) {
+                if (i % 2 != 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ 00. Recapitulação de Lógica de Programação/17. Os Ímpares/README.md
@@ -0,0 +1,22 @@
+# Exercício - Os Ímpares
+
+Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+
+### Exemplo de Entrada:
+
+```
+8
+```
+
+### Exemplo de Saída:
+
+```
+1
+3
+5
+7
+```
+
+---
+
+Acesse [aqui](https://github.com/JonathanBarr0s/Udemy-Java/blob/main/Se%C3%A7%C3%A3o%2006%20-%20Estruturas%20Repetitivas/04.%20Os%20%C3%8Dmpares/OsImpares/src/Main.java) minha resposta.
