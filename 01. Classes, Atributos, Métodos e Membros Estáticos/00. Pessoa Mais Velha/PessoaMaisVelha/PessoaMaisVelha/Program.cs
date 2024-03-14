afda19d7b498a9be5b8d2422ea4242ffad4cbe22
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2024-03-14 11:40:39 -0300

    Att

---
+++ 01. Classes, Atributos, Métodos e Membros Estáticos/00. Pessoa Mais Velha/PessoaMaisVelha/PessoaMaisVelha/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PessoaMaisVelha {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Pessoa primeiraPessoa = new Pessoa();
+            Pessoa segundaPessoa = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            primeiraPessoa.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            primeiraPessoa.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            segundaPessoa.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            segundaPessoa.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (primeiraPessoa.idade > segundaPessoa.idade) {
+
+                Console.WriteLine("Pessoa mais velha: {0}", primeiraPessoa.nome);
+
+            } else {
+                Console.WriteLine("Pessoa mais velha: {0}", segundaPessoa.nome);
+            }
+        }
+    }
+}
---
+++ 01. Classes, Atributos, Métodos e Membros Estáticos/00. Pessoa Mais Velha/README.md
@@ -0,0 +1,26 @@
+# Exercício - Pessoa Mais Velha
+
+Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais
+velha.
+
+### Exemplo de Entrada:
+
+```
+Dados da primeira pessoa:
+Nome: Maria
+Idade: 17
+
+Dados da segunda pessoa:
+Nome: Joao
+Idade: 16
+```
+
+### Exemplo de Saída:
+
+```
+Pessoa mais velha: Maria
+```
+
+---
+
+Acesse [aqui](https://github.com/JonathanBarr0s/Udemy-CSharp/blob/main/00.%20Recapitula%C3%A7%C3%A3o%20de%20L%C3%B3gica%20de%20Programa%C3%A7%C3%A3o/22.%20Testes%20de%20Matem%C3%A1tica/TestesDeMatematica/TestesDeMatematica/Program.cs) minha resposta.
