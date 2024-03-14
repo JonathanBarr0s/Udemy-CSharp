38e24d7db7dd8600248d86d613bd6412d28926f9
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2024-03-14 15:24:49 -0300

    Att

---
+++ 01. Introdução à Programação Orientada a Objetos/02. Dados do Retângulo/DadosDoRetangulo/DadosDoRetangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DadosDoRetangulo {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Retangulo retangulo = new Retangulo();          
+            
+            Console.Write("Digite a largura do retângulo: ");
+            retangulo.Largura = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura do retângulo: ");
+            retangulo.Altura= double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("AREA = {0}", retangulo.Area().ToString("F2"));
+            Console.WriteLine("PERÍMETRO = {0}", retangulo.Perimetro().ToString("F2"));
+            Console.WriteLine("DIAGONAL = {0}", retangulo.Diagonal().ToString("F2"));
+
+        }
+    }
+}
