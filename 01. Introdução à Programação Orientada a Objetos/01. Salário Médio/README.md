4e731cbdf379fdcca949b84f7d0716784afb3fb9
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2024-03-14 13:05:25 -0300

    Att

---
+++ 01. Introdução à Programação Orientada a Objetos/01. Salário Médio/README.md
@@ -0,0 +1,29 @@
+# Exercício - Salário Médio
+
+Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
+médio dos funcionários.
+
+### Exemplo de Entrada:
+
+```
+Dados do primeiro funcionário:
+Nome: Carlos Silva
+Salário: 6300.00
+
+Dados do segundo funcionário:
+Nome: Ana Marques
+Salário: 6700.00
+```
+
+### Exemplo de Saída:
+
+```
+Salário médio = 6500.00
+```
+
+---
+
+Minha resposta:
+
+- [Programa](https://github.com/JonathanBarr0s/Udemy-CSharp/blob/main/01.%20Introdu%C3%A7%C3%A3o%20%C3%A0%20Programa%C3%A7%C3%A3o%20Orientada%20a%20Objetos/00.%20Pessoa%20Mais%20Velha/PessoaMaisVelha/PessoaMaisVelha/Program.cs)
+- [Classe](https://github.com/JonathanBarr0s/Udemy-CSharp/blob/main/01.%20Introdu%C3%A7%C3%A3o%20%C3%A0%20Programa%C3%A7%C3%A3o%20Orientada%20a%20Objetos/00.%20Pessoa%20Mais%20Velha/PessoaMaisVelha/PessoaMaisVelha/Pessoa.cs)
---
+++ 01. Introdução à Programação Orientada a Objetos/01. Salário Médio/SalarioMedio/SalarioMedio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SalarioMedio {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            Funcionario primeiroFuncionario = new Funcionario();
+            Funcionario segundoFuncionario = new Funcionario();
+
+            double salarioMedio;
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            primeiroFuncionario.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            primeiroFuncionario.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            segundoFuncionario.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            segundoFuncionario.Salario = double.Parse(Console.ReadLine());
+
+            salarioMedio = (primeiroFuncionario.Salario + segundoFuncionario.Salario) / 2;
+
+            Console.WriteLine();
+
+            Console.WriteLine("Salário médio = {0}", salarioMedio);
+
+        }
+    }
+}
