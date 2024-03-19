89063bcc6fe1cc36f39bf1ca7f2c1416b0fb1369
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2024-03-19 16:36:31 -0300

    Att

---
+++ 02. Construtores, Palavra this, Sobrecarga e Encapsulamento/00. Banco/ContaBancaria/ContaBancaria/Banco.cs
@@ -0,0 +1,34 @@
+﻿namespace ContaBancaria {
+    internal class Banco {
+
+        public int NumeroConta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+
+        public Banco(int numeroConta, string titular) {
+            NumeroConta = numeroConta;
+            Titular = titular;
+        }
+
+        public Banco(int numeroConta, string titular, double depositoInicial) : this(numeroConta, titular) {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double saldo) {
+            Saldo += saldo;
+        }
+
+        public void Saque(double saldo) {
+            Saldo -= saldo + 5;
+        }
+
+        public void MudancaTitular(string titular) {
+            Titular = titular;
+        }
+
+        public override string ToString() {
+            return string.Format("Conta {0}, Titular: {1}, Saldo: $ {2:F2}", NumeroConta, Titular, Saldo);
+        }
+
+    }
+}
---
+++ 02. Construtores, Palavra this, Sobrecarga e Encapsulamento/00. Banco/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ContaBancaria {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Banco cliente;
+
+            Console.Write("Entre o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+
+            if (resposta == 's' || resposta == 'S') {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+
+                cliente = new Banco(numeroConta, titular, depositoInicial);
+            } else {
+                cliente = new Banco(numeroConta, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: " + cliente);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para depósito: ");
+            double transacao = double.Parse(Console.ReadLine());
+            cliente.Deposito(transacao);
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados:\nDados da conta: " + cliente);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para saque: ");
+            transacao = double.Parse(Console.ReadLine());
+            cliente.Saque(transacao);
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados:\nDados da conta: " + cliente);
+        }
+    }
+}
