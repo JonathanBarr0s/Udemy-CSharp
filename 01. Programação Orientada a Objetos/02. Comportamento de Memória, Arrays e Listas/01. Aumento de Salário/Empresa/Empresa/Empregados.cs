778904e2718f259e337fe31f1f39f135c2820c5d
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2024-03-21 15:31:10 -0300

    Att

---
+++ 01. Programação Orientada a Objetos/02. Comportamento de Memória, Arrays e Listas/01. Aumento de Salário/Empresa/Empresa/Empregados.cs
@@ -0,0 +1,22 @@
+﻿namespace Empresa {
+    internal class Empregado {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salario { get; set; }
+
+        public Empregado(int id, string name, double salario) {
+            Id = id;
+            Name = name;
+            Salario = salario;
+        }
+
+        public void AumentoSalario(double porcentagem) {
+            Salario = Salario * (1 + (porcentagem / 100));
+        }
+
+        public override string ToString() {
+            return string.Format("{0}, {1}, {2:F2}", Id, Name, Salario);
+        }
+    }
+}
---
+++ 01. Programação Orientada a Objetos/02. Comportamento de Memória, Arrays e Listas/01. Aumento de Salário/Empresa/Empresa/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Empresa {
+    internal class Program {
+        static void Main(string[] args) {
+
+            List<Empregado> listaEmpregados = new List<Empregado>();
+
+            Console.Write("Quantos funcionários serão cadastrados? ");
+            int quantidadeNovosCadastros = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= quantidadeNovosCadastros; i++) {
+                Console.WriteLine("Funcionário nº {0}:", i);
+                Console.Write("Identidade: ");
+                int identidade = int.Parse(Console.ReadLine());
+
+                Empregado verificandoSeIdJaExiste = listaEmpregados.Find(id => id.Id == identidade);
+
+                if (verificandoSeIdJaExiste == null) {
+                    Console.Write("Nome: ");
+                    string nome = Console.ReadLine();
+                    Console.Write("Salário: ");
+                    double salario = int.Parse(Console.ReadLine());
+                    listaEmpregados.Add(new Empregado(identidade, nome, salario));
+                    Console.WriteLine();
+                } else {
+                    while (verificandoSeIdJaExiste != null) {
+                        Console.WriteLine();
+                        Console.Write("Este ID já existe, digite outro: ");
+                        identidade = int.Parse(Console.ReadLine());
+                        verificandoSeIdJaExiste = listaEmpregados.Find(id => id.Id == identidade);
+                        Console.WriteLine();
+                    }
+                    Console.Write("Nome: ");
+                    string nome = Console.ReadLine();
+                    Console.Write("Salário: ");
+                    double salario = int.Parse(Console.ReadLine());
+                    listaEmpregados.Add(new Empregado(identidade, nome, salario));
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Informe o ID do funcionário que terá aumento salarial: ");
+            int idFuncionarioAumento = int.Parse(Console.ReadLine());
+
+            Empregado verificarSeIdExiste = listaEmpregados.Find(id => id.Id == idFuncionarioAumento);
+
+            if (verificarSeIdExiste != null) {
+                Console.Write("Digite a porcentagem de aumento: ");
+                double porcentagemAumentoSalarial = double.Parse(Console.ReadLine());
+                verificarSeIdExiste.AumentoSalario(porcentagemAumentoSalarial);
+            } else {
+                while (verificarSeIdExiste == null) {
+                    Console.WriteLine();
+                    Console.Write("Este ID não existe, digite outro: ");
+                    idFuncionarioAumento = int.Parse(Console.ReadLine());
+                    verificarSeIdExiste = listaEmpregados.Find(id => id.Id == idFuncionarioAumento);
+                }
+                Console.Write("Digite a porcentagem de aumento: ");
+                double porcentagemAumentoSalarial = double.Parse(Console.ReadLine());
+                verificarSeIdExiste.AumentoSalario(porcentagemAumentoSalarial);
+            }
+
+            Console.WriteLine();
+
+            foreach (Empregado item in listaEmpregados) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
