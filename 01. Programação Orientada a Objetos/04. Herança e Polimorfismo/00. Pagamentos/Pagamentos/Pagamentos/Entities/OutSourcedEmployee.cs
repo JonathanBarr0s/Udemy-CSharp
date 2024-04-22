1bca3fb60947cd9f25855ad2ebd3f7b6d713da4a
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2024-04-22 12:39:38 -0300

    Att

---
+++ 01. Programação Orientada a Objetos/04. Herança e Polimorfismo/00. Pagamentos/Pagamentos/Pagamentos/Entities/OutSourcedEmployee.cs
@@ -0,0 +1,17 @@
+﻿namespace Pagamentos.Entities {
+    internal class OutSourcedEmployee : Employee {
+
+        public double AdditionalCharge { get; set; }
+
+        public OutSourcedEmployee() {
+        }
+
+        public OutSourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base (name, hours, valuePerHour) {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment() {
+            return base.Payment() + (AdditionalCharge * 1.1);
+        }
+    }
+}
--- 01. Programação Orientada a Objetos/04. Herança e Polimorfismo/00. Pagamentos/Pagamentos/Pagamentos/Program.cs
+++ 01. Programação Orientada a Objetos/04. Herança e Polimorfismo/00. Pagamentos/Pagamentos/Pagamentos/Program.cs
@@ -1,7 +1,47 @@
-﻿namespace Pagamentos {
+﻿using Pagamentos.Entities;
+using System.Collections.Generic;
+using System;
+
+namespace Pagamentos {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int numberOfEmployees = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= numberOfEmployees; i++) {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char outsourced = char.Parse(Console.ReadLine().ToLower());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+
+                if (outsourced == 'y') {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine());
+
+                    OutSourcedEmployee outSourcedEmployee = new OutSourcedEmployee(name, hours, valuePerHour, additionalCharge);
+                    employees.Add(outSourcedEmployee);
+                } else {
+                    Employee employee = new Employee(name, hours, valuePerHour);
+                    employees.Add(employee);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("PAYMENTS:");
+            foreach (Employee employee in employees) {
+                Console.WriteLine($"{employee.Name} - R$ {employee.Payment().ToString("F2")}");
+            }
         }
     }
 }
