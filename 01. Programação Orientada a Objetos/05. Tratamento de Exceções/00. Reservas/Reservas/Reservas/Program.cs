a3bd2cd70d42e1ce56821593939258b115f6be81
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2024-04-25 16:13:48 -0300

    Att

--- 01. Programação Orientada a Objetos/05. Tratamento de Exceções/00. Reservas/Reservas/Reservas/Program.cs
+++ 01. Programação Orientada a Objetos/05. Tratamento de Exceções/00. Reservas/Reservas/Reservas/Program.cs
@@ -1,32 +1,40 @@
 ﻿using Reservas.Entities;
+using Reservas.Entities.Exceptions;
 
 namespace Reservas {
     internal class Program {
         static void Main(string[] args) {
 
-            Console.Write("Room number: ");
-            int roomNumber = int.Parse(Console.ReadLine());
-            Console.Write("Check-in date (dd/MM/yyyy): ");
-            DateTime checkIn = DateTime.Parse(Console.ReadLine());
-            Console.Write("Check-out date (dd/MM/yyyy): ");
-            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+            try {
+                Console.Write("Room number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
 
-            Console.WriteLine();
-            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine();
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 
-            Console.WriteLine(reservation);
-            Console.WriteLine();
+                Console.WriteLine(reservation);
+                Console.WriteLine();
 
-            Console.WriteLine("Enter data to update the reservation:");
-            Console.Write("Check-in date (dd/MM/yyyy): ");
-            checkIn = DateTime.Parse(Console.ReadLine());
-            Console.Write("Check-out date (dd/MM/yyyy): ");
-            checkOut = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
 
-            Console.WriteLine();
-            reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine();
+                reservation.UpdateDates(checkIn, checkOut);
 
-            Console.WriteLine(reservation);
+                Console.WriteLine(reservation);
+
+            } catch (DomainException exception) {
+
+                Console.WriteLine($"Error in reservation: {exception.Message}");
+            }
+
         }
     }
 }
