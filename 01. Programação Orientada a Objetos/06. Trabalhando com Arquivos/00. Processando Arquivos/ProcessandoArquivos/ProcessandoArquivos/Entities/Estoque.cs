c99c060309c723a7f894421c9e9ece865e729c76
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2024-05-07 17:31:38 -0300

    Att

---
+++ 01. Programação Orientada a Objetos/06. Trabalhando com Arquivos/00. Processando Arquivos/ProcessandoArquivos/ProcessandoArquivos/Entities/Estoque.cs
@@ -0,0 +1,18 @@
+﻿namespace ProcessandoArquivos {
+    internal class Estoque {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Estoque(string name, double price, int quantity) {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total() {
+            return Price * Quantity;
+        }
+    }
+}
--- 01. Programação Orientada a Objetos/06. Trabalhando com Arquivos/00. Processando Arquivos/ProcessandoArquivos/ProcessandoArquivos/Program.cs
+++ 01. Programação Orientada a Objetos/06. Trabalhando com Arquivos/00. Processando Arquivos/ProcessandoArquivos/ProcessandoArquivos/Program.cs
@@ -1,7 +1,41 @@
-﻿namespace ProcessandoArquivos {
+﻿using System.Globalization;
+
+namespace ProcessandoArquivos {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            string path = @"D:\Bibliotecas do Windows\Downloads\Arquivo.txt";
+            string newPath = path + @"\out";
+
+            try {
+
+                string[] allLines = File.ReadAllLines(path);
+
+                string sourceFolderPath = Path.GetDirectoryName(path);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.txt";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath)) {
+                    foreach (string line in allLines) {
+
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Estoque estoque = new Estoque(name, price, quantity);
+
+                        sw.WriteLine(estoque.Name + "," + estoque.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+
+            } catch (IOException exception) {
+
+                Console.WriteLine("Ocorreu o seguinte erro: " + exception.Message);
+            }
+
         }
     }
 }
