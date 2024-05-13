4df6526f12f398649b3bb7767312a5782d308288
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2024-05-13 00:12:16 -0300

    Att

--- 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/00. Numeros/Numeros/Numeros/Program.cs
+++ 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/00. Numeros/Numeros/Numeros/Program.cs
@@ -1,11 +1,23 @@
-﻿namespace Numeros {
+﻿using Numeros.Services;
+
+namespace Numeros {
     internal class Program {
         static void Main(string[] args) {
+            
+            ServicoImpressao<string> servicoImpressao = new ServicoImpressao<string>();
 
-            Console.WriteLine("Quantos valores? ");
+            Console.Write("Quantos valores? ");
             int quantidadeValores = int.Parse(Console.ReadLine());
 
+            for (int i = 1; i <= quantidadeValores; i++) {
+                
+                string valor = Console.ReadLine();
+                servicoImpressao.AdicionarValores(valor);
+            }
 
+            Console.WriteLine();
+            servicoImpressao.Imprimir();
+            servicoImpressao.Primeiro();
         }
     }
 }
---
+++ 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/00. Numeros/Numeros/Numeros/Services/ServicoImpressao.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Numeros.Services {
+    internal class ServicoImpressao<T> {
+
+        public T[] valores = new T[10];
+        public int contador = 0;
+
+        public ServicoImpressao() {
+        }
+
+        public void AdicionarValores(T valor) {
+
+            valores[contador] = valor;
+            contador++;
+        }
+
+        public void Primeiro() {
+            Console.WriteLine("Primeiro: " + valores[0]);
+        }
+
+        public void Imprimir() {
+
+            Console.Write("[");
+
+            for (int i = 0; i <= contador - 2; i++) {
+                Console.Write(valores[i] + ", ");
+            }
+
+            Console.WriteLine(valores[contador - 1] + "]");
+        }
+    }
+}
