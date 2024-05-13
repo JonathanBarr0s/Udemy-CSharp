64fdc6a476259d79388a771bb5057f68b7a2ccf0
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2024-05-12 22:11:03 -0300

    Att

--- 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/00. Numeros/Numeros/Numeros/Program.cs
+++ 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/00. Numeros/Numeros/Numeros/Program.cs
@@ -1,7 +1,11 @@
 ﻿namespace Numeros {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Quantos valores? ");
+            int quantidadeValores = int.Parse(Console.ReadLine());
+
+
         }
     }
 }
