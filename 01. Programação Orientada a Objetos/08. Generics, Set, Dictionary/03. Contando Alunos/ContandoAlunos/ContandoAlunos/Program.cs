c70f3cca6aa55f9f5ad44d81ff79f3e82c5caf17
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2024-05-14 15:47:52 -0300

    Att

--- 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/03. Contando Alunos/ContandoAlunos/ContandoAlunos/Program.cs
+++ 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/03. Contando Alunos/ContandoAlunos/ContandoAlunos/Program.cs
@@ -1,7 +1,22 @@
 ﻿namespace ContandoAlunos {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            HashSet<int> alunosPorMatricula = new HashSet<int>();
+
+            string[] cursos = { "A", "B", "C" };
+
+            foreach (string curso in cursos) {
+                Console.Write($"Quanto alunos para o curso {curso}? ");
+                int quantidadeAlunos = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= quantidadeAlunos; i++) {
+                    int matricula = int.Parse(Console.ReadLine());
+                    alunosPorMatricula.Add(matricula);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Total de estudantes: {alunosPorMatricula.Count()}");
         }
     }
 }
