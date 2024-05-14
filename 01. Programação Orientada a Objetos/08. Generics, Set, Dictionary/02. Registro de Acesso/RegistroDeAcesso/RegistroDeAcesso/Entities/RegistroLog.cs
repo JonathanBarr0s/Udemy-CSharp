d20bf277f08879a2d3a7ca5a0302a8901d392106
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2024-05-14 14:25:43 -0300

    Att

---
+++ 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/02. Registro de Acesso/RegistroDeAcesso/RegistroDeAcesso/Entities/RegistroLog.cs
@@ -0,0 +1,19 @@
+﻿namespace RegistroDeAcesso.Entities {
+    internal class RegistroLog {
+
+        public string Nome { get; set; }
+        public DateTime Instante { get; set; }
+
+        public override int GetHashCode() {
+            return Nome.GetHashCode();
+        }
+
+        public override bool Equals(object? obj) {
+            if (!(obj is RegistroLog)) {
+                return false;
+            }
+            RegistroLog outro = obj as RegistroLog;
+            return Nome.Equals(outro.Nome);
+        }
+    }
+}
--- 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/02. Registro de Acesso/RegistroDeAcesso/RegistroDeAcesso/Program.cs
+++ 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/02. Registro de Acesso/RegistroDeAcesso/RegistroDeAcesso/Program.cs
@@ -1,7 +1,29 @@
-﻿namespace RegistroDeAcesso {
+﻿using RegistroDeAcesso.Entities;
+
+namespace RegistroDeAcesso {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            HashSet<RegistroLog> set = new HashSet<RegistroLog>();
+
+            string path = @"D:\Bibliotecas do Windows\Downloads\in.txt";
+
+            try {
+
+                using (StreamReader sr = File.OpenText(path)) {
+
+                    while (!sr.EndOfStream) {
+                        string[] linha = sr.ReadLine().Split(' ');
+                        string nome = linha[0];
+                        DateTime instante = DateTime.Parse(linha[1]);
+                        set.Add(new RegistroLog { Instante = instante, Nome = nome });
+                    }
+                    Console.WriteLine($"Total de usuários: {set.Count}");
+                }
+
+            } catch (IOException exception) {
+                Console.WriteLine(exception.Message);
+            }
         }
     }
 }
