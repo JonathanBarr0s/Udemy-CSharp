19657d2b2a1ecd62a6e9df23ad45088ab63f9043
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2024-05-14 17:56:15 -0300

    Att

--- 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/04. Contagem de Votos/ContagemDeVotos/ContagemDeVotos/Program.cs
+++ 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/04. Contagem de Votos/ContagemDeVotos/ContagemDeVotos/Program.cs
@@ -1,7 +1,35 @@
 ﻿namespace ContagemDeVotos {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            string path = @"D:\Bibliotecas do Windows\Downloads\in.txt";
+
+            try {
+
+                Dictionary<string, int> candidatosEVotos = new Dictionary<string, int>();
+
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] linha = sr.ReadLine().Split(',');
+                        string nome = linha[0];
+                        int votos = int.Parse(linha[1]);
+
+                        if (candidatosEVotos.ContainsKey(nome)) {
+                            candidatosEVotos[nome] += votos;
+                        } else {
+                            candidatosEVotos[nome] = votos;
+                        }
+                    }
+                }
+
+                foreach (var candidato in candidatosEVotos) {
+                    Console.WriteLine(candidato.Key + ": " + candidato.Value);
+                }
+
+            } catch (Exception exception) {
+
+                Console.WriteLine("Erro: " + exception.Message);
+            }
         }
     }
 }
