b3a1d12be03f1ec497a34c5da20328e8a1563ac5
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2024-05-14 17:05:53 -0300

    Att

--- 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/03. Contando Alunos/ContandoAlunos/ContandoAlunos/Program.cs
+++ 01. Programação Orientada a Objetos/08. Generics, Set, Dictionary/03. Contando Alunos/ContandoAlunos/ContandoAlunos/Program.cs
@@ -2,7 +2,9 @@
     internal class Program {
         static void Main(string[] args) {
 
-            HashSet<int> alunosPorMatricula = new HashSet<int>();
+            HashSet<int> cursoA = new HashSet<int>();
+            HashSet<int> cursoB = new HashSet<int>();
+            HashSet<int> cursoC = new HashSet<int>();            
 
             string[] cursos = { "A", "B", "C" };
 
@@ -10,13 +12,35 @@
                 Console.Write($"Quanto alunos para o curso {curso}? ");
                 int quantidadeAlunos = int.Parse(Console.ReadLine());
 
-                for (int i = 1; i <= quantidadeAlunos; i++) {
-                    int matricula = int.Parse(Console.ReadLine());
-                    alunosPorMatricula.Add(matricula);
+                if (curso == "A") {
+                    for (int i = 1; i <= quantidadeAlunos; i++) {
+                        int matricula = int.Parse(Console.ReadLine());
+                        cursoA.Add(matricula);
+                    }
+                    Console.WriteLine();
+
+                } else if (curso == "B") {
+                    for (int i = 1; i <= quantidadeAlunos; i++) {
+                        int matricula = int.Parse(Console.ReadLine());
+                        cursoB.Add(matricula);
+                    }
+                    Console.WriteLine();
+
+                } else if (curso == "C") {
+                    for (int i = 1; i <= quantidadeAlunos; i++) {
+                        int matricula = int.Parse(Console.ReadLine());
+                        cursoC.Add(matricula);
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
             }
-            Console.WriteLine($"Total de estudantes: {alunosPorMatricula.Count()}");
+
+            HashSet<int> total = new HashSet<int>(cursoA);
+
+            total.UnionWith(cursoB);
+            total.UnionWith(cursoC);
+
+            Console.WriteLine($"Total de estudantes: {total.Count()}");
         }
     }
 }
