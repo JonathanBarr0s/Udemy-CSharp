0dc3acc26aca4407396cad715c6b2c50b856537d
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2024-05-18 21:31:00 -0300

    Att

--- 01. Programação Orientada a Objetos/09. Expressões Lambda, Delegates, LINQ/00. Filtrando Produtos/FiltrandoProdutos/FiltrandoProdutos/Program.cs
+++ 01. Programação Orientada a Objetos/09. Expressões Lambda, Delegates, LINQ/00. Filtrando Produtos/FiltrandoProdutos/FiltrandoProdutos/Program.cs
@@ -1,7 +1,34 @@
-﻿namespace FiltrandoProdutos {
+﻿using FiltrandoProdutos.Entities;
+using System.Globalization;
+
+namespace FiltrandoProdutos {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            List<Produto> produtos = new List<Produto>();
+
+            string path = @"D:\Bibliotecas do Windows\Downloads\in.txt";
+
+            using (StreamReader sr = File.OpenText(path)) {
+
+                while (!sr.EndOfStream) {
+                    string[] linha = sr.ReadLine().Split(',');
+                    string nome = linha[0];
+                    double preco = double.Parse(linha[1], CultureInfo.InvariantCulture);
+                    produtos.Add(new Produto(nome, preco));
+                }
+            }
+
+            var media = produtos.Select(produto => produto.Preco).Average();
+            Console.WriteLine($"Preço médio: R$ {media.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine();
+
+            var nomes = produtos.Where(produto => produto.Preco < media).OrderByDescending(produto => produto.Nome).Select(produto => produto.Nome);
+
+            foreach (var produto in nomes) {
+                Console.WriteLine(produto);
+            }
         }
     }
 }
